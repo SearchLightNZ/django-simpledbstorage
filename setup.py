97a099391e6f92993cc6ba9bda11cf90f3142692
Nathan Ward <nward@braintrust.co.nz>
2019-06-08 16:42:21 +1200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-simpledbstorage',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='A simple Django app to store files in a db.',
     long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/SearchLightNZ/django-simpledbstorage',
     author='Nathan Ward',
     author_email='nward@serachlight.nz',
